1c51706fc1557acdb938303eb653de368aa8ae1b
Mohamed Mostafa <thepcvirus@gmail.com>
2025-05-29 10:57:25 +0200

    Added Low Res Functionality to Index Page, Copied project Files to Public/ for hosting

---
+++ public/ConfigFile.js
@@ -0,0 +1,26 @@
+export const firebaseConfig = {
+    // apiKey: "AIzaSyCPA5xNO3YPwtJSVrYHCxFCmIkMYkhdhWc",
+    // authDomain: "js-project-92c8d.firebaseapp.com",
+    // projectId: "js-project-92c8d",
+    // storageBucket: "js-project-92c8d.firebasestorage.app",
+    // messagingSenderId: "629295037407",
+    // appId: "1:629295037407:web:d929a43135e6131cd4226a"
+    // osama configration
+    apiKey: "AIzaSyAEpHidRTkCegpQmOzHy6hV7iApvAJIFoM",
+    authDomain: "test-de06d.firebaseapp.com",
+    projectId: "test-de06d",
+    storageBucket: "test-de06d.firebasestorage.app",
+    messagingSenderId: "308036633924",
+    appId: "1:308036633924:web:1894faeee93a0815b14311",
+    measurementId: "G-XKSNCZ34EL",
+};
+
+export const imagesfirebaseConfig = {
+    apiKey: "AIzaSyB7Lw4lts6ldobMELZ0HLiuPd4WK_uirmA",
+    authDomain: "jsecommerce-9350b.firebaseapp.com",
+    projectId: "jsecommerce-9350b",
+    storageBucket: "jsecommerce-9350b.firebasestorage.app",
+    messagingSenderId: "1010783396973",
+    appId: "1:1010783396973:web:f34f9b12f4bf16c579cb64",
+    measurementId: "G-RHVY9CVE46"
+};
